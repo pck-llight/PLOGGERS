eabdab79e2e5375155abee43c07cab4b75d1a941
pck-llight <23sunrin113@sunrint.hs.kr>
2023-08-16 11:12:15 +0900

    타이머 구현중

--- app/src/main/java/com/example/ploggers/plogging_timer.kt
+++ app/src/main/java/com/example/ploggers/plogging_timer.kt
@@ -2,10 +2,37 @@ package com.example.ploggers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ploggers.databinding.ActivityCreatePostBinding
+import com.example.ploggers.databinding.ActivityMainBinding
+import com.example.ploggers.databinding.ActivityPloggingTimerBinding
 
 class plogging_timer : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPloggingTimerBinding
+    private var isRunning = false
+    private var runTime = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plogging_timer)
+
+        binding = ActivityPloggingTimerBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.timerButton.setOnClickListener {
+            if(isRunning){
+            }
+            else{
+
+            }
+        }
+
     }
+
+    private fun startTimer(){
+
+    }
+
+
 }
