c39f9cb068094c70329b2ee73b09ba6f652781d1
skwpwns <129128579+skwpwns@users.noreply.github.com>
2023-08-16 23:45:29 +0900

    파이어베이스 연동 XML 오류 수정

--- app/src/main/java/com/example/ploggers/sign_up.kt
+++ app/src/main/java/com/example/ploggers/sign_up.kt
@@ -2,10 +2,17 @@ package com.example.ploggers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ploggers.databinding.ActivityLoginBinding
+import com.example.ploggers.databinding.ActivitySignUpBinding
+import com.example.ploggers.databinding.ActivityStartBinding
 
 class sign_up : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
