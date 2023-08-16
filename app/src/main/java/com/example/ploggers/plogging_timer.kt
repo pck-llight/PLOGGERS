c6fcfe00b00a27f41e5d86ee8ff04a1bbbbd4dee
ll-chankyu <23sunrin113@sunrint.hs.kr>
2023-08-16 14:31:20 +0900

    타이머 기능 수정

--- app/src/main/java/com/example/ploggers/plogging_timer.kt
+++ app/src/main/java/com/example/ploggers/plogging_timer.kt
@@ -33,39 +33,36 @@ class plogging_timer : AppCompatActivity() {
             reset()
         }
 
-        binding.timer1.setText("$time")
-
 
     }
 
-    private var min = time/60
-    private var sec = time
 
     private fun start() {
         timerTask = kotlin.concurrent.timer(period = 1000) {	// timer() 호출
             time++	// period=1000, 1초마다 time를 1씩 증가
             isRunning = true
 
-
+            var sec = time
+            var min = sec/60
+            var hour = min/60
 
             runOnUiThread {
-                binding.timer1?.text = "${min} : ${sec}"
+                binding.timer1?.text = "${hour} : ${min%60} : ${sec%60}"
             }
         }
     }
 
     private fun pause() {
-        timerTask?.cancel();	// 안전한 호출(?.)로 timerTask가 null이 아니면 cancel() 호출
+        timerTask?.cancel()	// 안전한 호출(?.)로 timerTask가 null이 아니면 cancel() 호출
         isRunning = false
     }
 
     private fun reset() {
-        timerTask?.cancel()	// timerTask가 null이 아니라면 cancel() 호출
         time = 0		// 시간저장 변수 초기화
         isRunning = false	// 현재 진행중인지 판별하기 위한 Boolean변수 false 세팅
 
         runOnUiThread {
-            binding.timer1?.text = "${min} : ${sec}"
+            binding.timer1?.text = "0 : 0 : 0"
         }
     }
 
