f0f551e1b36fdfad13d357cb0a2077bf8958e70d
pck-llight <23sunrin113@sunrint.hs.kr>
2023-08-22 22:40:56 +0900

    파이어 베이스 라이브러리 추가

--- app/src/main/java/com/example/ploggers/MainActivity.kt
+++ app/src/main/java/com/example/ploggers/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.ploggers
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.example.ploggers.databinding.ActivityMainBinding
 import com.example.ploggers.databinding.ActivityPloggingTimerBinding
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    private val fbStorage = FirebaseStorage.getInstance()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,13 +20,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val intent = Intent(this, ploggingtimer::class.java)
 
-
         binding.timerStartButton.setOnClickListener() {
             startActivity(intent)
             Log.d("Df", "Sdf")
         }
 
-
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_MEDIA_IMAGES),101)
 
     }
 }
